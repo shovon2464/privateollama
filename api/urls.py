ef780d9a729be14c4965605ef2a8e6eab88f650e
Shovon Bhowmick <shovon2464@gmail.com>
2024-05-29 16:10:37 -0600

    classify natures is written for testing

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IsActiveView
+from .views import IsActiveView,ClassifyNaturesView
 
 urlpatterns = [
     path('isactive/', IsActiveView.as_view(), name='is-active'),
+    path('classifynatures/', ClassifyNaturesView.as_view(), name='classify-natures'),
 ]
--- api/views.py
+++ api/views.py
@@ -11,7 +11,48 @@ import requests
 import json
 # Create your views here.
 
+from unsloth import FastLanguageModel
+import torch
+
 class IsActiveView(APIView):
     def get(self, request):
         data = {"message": "I Am Active, I Am Private Ollama!"}
         return Response(data, status=status.HTTP_200_OK)
+    
+    
+
+@method_decorator(csrf_exempt, name='dispatch')
+class ClassifyNaturesView(View):
+    #using privateollma
+    def post(self, request):
+        try:
+            prompt = request.POST.get('document')
+            max_seq_length = 2248 # Choose any! We auto support RoPE Scaling internally!
+            dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+            load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+            model, tokenizer = FastLanguageModel.from_pretrained(
+                    model_name = "classify_natures", # YOUR MODEL YOU USED FOR TRAINING
+                    max_seq_length = max_seq_length,
+                    dtype = dtype,
+                    load_in_4bit = load_in_4bit,
+                )  
+            instruction = "Classify the following insurance document into one of the specified categories based on its content. Return the classification as a JSON object with the type in short form. Do not include reasoning. Example output: {\n\"type\": \"PCH \"}.If the document describes a New Business then classify it to NBS. If the document describes Cancellation of policy then classify it to XLN. If the document changes the policy then classify it to PCH.  Be careful not to confuse this with NBS If the document is a reminder of an outstanding balance and time to pay, classify it as ACR. If the document describes an Endorsement then classify it to EDT. .If the document is a debit note describing a transaction then classify it to DBR.If the document describes Renewal then classify it to RWL. If the document indicates that a renewal was issued, the client is not happy, and a change is needed, classify it as RII. If the document describes reinstatement after cancellation due to non-payment, classify it as REI. Only return the classification in the specified JSON format."
+
+            alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+            ### Instruction:
+            {}
+
+            ### Input:
+            {}
+
+            ### Response:
+            {}"""
+            response = {"message":"hi"}
+            response = response.json()
+            return JsonResponse(response,safe=False)
+
+
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=400)
+        
