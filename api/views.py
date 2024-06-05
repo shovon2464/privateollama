508b88c333bac2c26f990c1077f022990f4bb906
Shovon Bhowmick <shovon2464@gmail.com>
2024-06-05 16:41:52 -0600

    gpu memory management has been rectified

--- api/views.py
+++ api/views.py
@@ -9,19 +9,10 @@ from django.utils.decorators import method_decorator
 from django.views import View
 import requests
 import json
+from numba import cuda
 # Create your views here.
 
-from unsloth import FastLanguageModel
-import torch
-max_seq_length = 9000 # Choose any! We auto support RoPE Scaling internally!
-dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
-model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "classify_natures_6600l", # YOUR MODEL YOU USED FOR TRAINING
-        max_seq_length = max_seq_length,
-        dtype = dtype,
-        load_in_4bit = load_in_4bit,
-    ) 
+
  
 
 
@@ -48,6 +39,17 @@ class ClassifyNaturesView(View):
     #using privateollma
     def post(self, request):
         try:
+            from unsloth import FastLanguageModel
+            import torch
+            max_seq_length = 9000 # Choose any! We auto support RoPE Scaling internally!
+            dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+            load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+            model, tokenizer = FastLanguageModel.from_pretrained(
+                    model_name = "natures_classify_6600l", # YOUR MODEL YOU USED FOR TRAINING
+                    max_seq_length = max_seq_length,
+                    dtype = dtype,
+                    load_in_4bit = load_in_4bit,
+                    ) 
             content = request.POST.get('document')
             instruction = "Classify the following insurance document into one of the specified categories based on its content. Return the classification as a JSON object with the type in short form. Do not include reasoning. Example output: {\n\"type\": \"PCH \"}.If the document describes a New Business then classify it to NBS. If the document describes Cancellation of policy then classify it to XLN. If the document changes the policy then classify it to PCH.  Be careful not to confuse this with NBS If the document is a reminder of an outstanding balance and time to pay, classify it as ACR. If the document describes an Endorsement then classify it to EDT. .If the document is a debit note describing a transaction then classify it to DBR.If the document describes Renewal then classify it to RWL. If the document indicates that a renewal was issued, the client is not happy, and a change is needed, classify it as RII. If the document describes reinstatement after cancellation due to non-payment, classify it as REI. Only return the classification in the specified JSON format."
             inputs = tokenizer(
@@ -57,6 +59,8 @@ class ClassifyNaturesView(View):
 
             outputs = model.generate(**inputs, max_new_tokens = 64, use_cache = True)
             response = tokenizer.batch_decode(outputs)
+            device = cuda.get_current_device()
+            device.reset()
             response = response[0].split("###")
             response = response[3]
             start_index = response.find('{') 
